adb4e9cb5dc5ab941a39cf5cd176815aca3c809f
jamesl1001 <jamesl1001@msn.com>
2014-04-29 21:26:27 +0100

    Add/Improve hover states to all buttons. Full-width bkgs.

--- views/index.php
+++ views/index.php
@@ -1,23 +1,29 @@
-<header class="centre">
-    <img src="/img/logo.png" alt="Workshop Queue logo" id="header-img"/>
-    <div id="header-info">
-        <h1 id="header-title">Workshop Queue</h1>
-        <p>Explain what WQ is here...</p>
+<header>
+    <div class="centre">
+        <img src="/img/logo.png" alt="Workshop Queue logo" id="header-img"/>
+        <div id="header-info">
+            <h1 id="header-title">Workshop Queue</h1>
+            <p>Explain what WQ is here...</p>
+        </div>
     </div>
 </header>
 
-<div id="main" class="centre">
-    <h2>Which workshop are you in?</h2>
+<div id="main">
+    <div class="centre">
+        <h2>Which workshop are you in?</h2>
 
-    <?php foreach($workshops as $workshop): ?>
+        <div id="workshop-cards">
+            <?php foreach($workshops as $workshop): ?>
 
-        <a href="/workshop/<?= $workshop->workshopId; ?>" class="workshop-card">
-            <img src="/img/<?= $workshop->type; ?>.png" alt="<?= $workshop->type; ?>"/>
-            <span class="workshop-card-title"><?= $workshop->module; ?></span>
-            <span class="workshop-card-lecturer"><?= $workshop->lecturer; ?></span>
-            <span class="workshop-card-date"><?= $workshop->date; ?></span>
-            <span class="workshop-card-room"><?= $workshop->room; ?></span>
-        </a>
+                <a href="/workshop/<?= $workshop->workshopId; ?>" class="workshop-card">
+                    <img src="/img/<?= $workshop->type; ?>.png" alt="<?= $workshop->type; ?>"/>
+                    <span class="workshop-card-title"><?= $workshop->module; ?></span>
+                    <span class="workshop-card-lecturer"><?= $workshop->lecturer; ?></span>
+                    <span class="workshop-card-date"><?= $workshop->date; ?></span>
+                    <span class="workshop-card-room"><?= $workshop->room; ?></span>
+                </a>
 
-    <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>
--- views/workshop.php
+++ views/workshop.php
@@ -1,50 +1,53 @@
-<header class="centre">
-    <img src="/img/<?= $workshop->type; ?>.png" alt="<?= $workshop->type; ?>" id="header-img"/>
-    <div id="header-info">
-        <h1 id="header-title"><?= $workshop->module; ?></h1>
-        <div class="full-width">
-            <p id="workshop-lecturer"><?= $workshop->lecturer; ?></p>
-            <p id="workshop-date"><?= $workshop->date; ?></p>
+<header>
+    <div class="centre">
+        <img src="/img/<?= $workshop->type; ?>.png" alt="<?= $workshop->type; ?>" id="header-img"/>
+        <div id="header-info">
+            <h1 id="header-title"><?= $workshop->module; ?></h1>
+            <div class="full-width">
+                <p id="workshop-lecturer"><?= $workshop->lecturer; ?></p>
+                <p id="workshop-date"><?= $workshop->date; ?></p>
+            </div>
+            <div class="full-width">
+                <p id="workshop-room"><i class="icon-pc"></i> <?= $workshop->room; ?></p>
+                <p id="workshop-users"><i class="icon-user"></i> <span id="workshop-slot-count"><?= count($slots); ?></span></p>
+                <p id="workshop-time"><i class="icon-time"></i> <?= $workshop->time; ?> mins</p>
+            </div>
         </div>
-        <div class="full-width">
-            <p id="workshop-room"><i class="icon-pc"></i> <?= $workshop->room; ?></p>
-            <p id="workshop-users"><i class="icon-user"></i> <span id="workshop-slot-count"><?= count($slots); ?></span></p>
-            <p id="workshop-time"><i class="icon-time"></i> <?= $workshop->time; ?> mins</p>
-        </div>
+        <a href="/" id="exit-btn" title="Exit"><i class="icon-exit"></i></a>
     </div>
-    <a href="/" id="exit-btn" title="Exit"><i class="icon-exit"></i></a>
 </header>
 
-<div id="main" class="centre">
+<div id="main">
+    <div class="centre">
+        <div id="user-slots"></div>
 
-    <div id="user-slots"></div>
+        <label id="request-assistance-btn" for="request-assistance-toggle"><i class="icon-add"></i><span>Request assistance</span></label>
+        <input type="checkbox" id="request-assistance-toggle" class="checkbox-hack"/>
 
-    <label id="request-assistance-btn" for="request-assistance-toggle"><i class="icon-add"></i><span>Request assistance</span></label>
-    <input type="checkbox" id="request-assistance-toggle" class="checkbox-hack"/>
+        <div id="request-assistance-modal">
+            <form>
+                <div class="request-assistance-form-row">
+                    <label for="request-name">Name</label> <input type="text" id="request-name" required/>
+                </div>
 
-    <div id="request-assistance-modal">
-        <form>
-            <div class="request-assistance-form-row">
-                <label for="request-name">name</label> <input type="text" id="request-name" required/>
-            </div>
+                <div class="request-assistance-form-row">
+                    <label for="request-id">Kent ID</label> <input type="text" id="request-id" required/>
+                </div>
 
-            <div class="request-assistance-form-row">
-                <label for="request-id">kent ID</label> <input type="text" id="request-id" required/>
-            </div>
+                <div class="request-assistance-form-row">
+                    <label for="request-seat">Seat</label> <img src="/img/room-<?= $workshop->room; ?>.png"/>
+                </div>
 
-            <div class="request-assistance-form-row">
-                <label for="request-seat">seat</label> <img src="/img/room-<?= $workshop->room; ?>.png"/>
-            </div>
+                <div class="request-assistance-form-row">
+                    <i class="icon-add"></i><input type="submit" value="done" id="request-submit"/>
+                </div>
+            </form>
+        </div>
 
-            <div class="request-assistance-form-row">
-                <i class="icon-add"></i><input type="submit" value="done" id="request-submit"/>
-            </div>
-        </form>
-    </div>
-
-    <div id="map-modal">
-        <h2><?= $workshop->room; ?></h2>
-        <img src="/img/room-<?= $workshop->room; ?>.png" title="<?= $workshop->room; ?>"/>
-        <div id="map-modal-marker"></div>
+        <div id="map-modal">
+            <h2><?= $workshop->room; ?></h2>
+            <img src="/img/room-<?= $workshop->room; ?>.png" title="<?= $workshop->room; ?>"/>
+            <div id="map-modal-marker"></div>
+        </div>
     </div>
 </div>
