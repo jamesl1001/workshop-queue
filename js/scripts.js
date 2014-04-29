6c03448cf84c009cc083c29b1dbbc878858ad8f3
jamesl1001 <jamesl1001@msn.com>
2014-04-29 18:00:17 +0100

    JS tidy-up

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
-// Get slots
-var i = 1;
+// Init
+var main             = document.getElementById('main');
 var userSlotsWrapper = document.getElementById('user-slots');
+var mapModal         = document.getElementById('map-modal');
+var mapModalMarker   = document.getElementById('map-modal-marker');
+
+getSlots();
+
+// Poll for slot changes
+var i = 1;
 
 setInterval(function() {
     console.log('Polling... [' + i + ']');
@@ -8,6 +15,28 @@ setInterval(function() {
     i++;
 }, 5000);
 
+// Listeners
+userSlotsWrapper.addEventListener('click', function(e) {
+    if(e.target.className == 'user-name') {
+        showMap(e.target.parentElement.getAttribute('data-location'));
+    }
+
+    if(e.target.className == 'user-id') {
+        showMap(e.target.parentElement.parentElement.getAttribute('data-location'));
+    }
+
+    if(e.target.className == 'user-cancel') {
+        cancelSlot(e.target.parentElement.getAttribute('data-slotid'));
+    }
+
+    if(e.target.className == 'icon-cancel') {
+        cancelSlot(e.target.parentElement.parentElement.getAttribute('data-slotid'));
+    }
+});
+
+mapModal.addEventListener('click', hideMap);
+
+// Events
 function getSlots() {
     var data = 'workshopId=' + workshopId;
     var request = new XMLHttpRequest();
@@ -27,33 +56,6 @@ function getSlots() {
     }
 }
 
-getSlots();
-
-// Show map
-userSlotsWrapper.addEventListener('click', function(e) {
-    if(e.target.className == 'user-name') {
-        showMap(e.target.parentElement.getAttribute('data-location'));
-    }
-
-    if(e.target.className == 'user-id') {
-        showMap(e.target.parentElement.parentElement.getAttribute('data-location'));
-    }
-
-    if(e.target.className == 'user-cancel') {
-        cancelSlot(e.target.parentElement.getAttribute('data-slotid'));
-    }
-
-    if(e.target.className == 'icon-cancel') {
-        cancelSlot(e.target.parentElement.parentElement.getAttribute('data-slotid'));
-    }
-});
-
-var main           = document.getElementById('main');
-var mapModal       = document.getElementById('map-modal');
-var mapModalMarker = document.getElementById('map-modal-marker');
-
-mapModal.addEventListener('click', hideMap);
-
 function showMap(location) {
     main.className += ' map-modal--show';
     mapModalMarker.innerHTML = location;
@@ -63,9 +65,8 @@ function hideMap() {
     main.className = main.className.replace(' map-modal--show', '');
 }
 
+// Cancel slot
 function cancelSlot(slotId) {
-    console.log(slotId);
-
     var data = 'slotId=' + slotId;
     var request = new XMLHttpRequest();
     request.open('POST', '/php/cancelSlot.php', true);
@@ -74,7 +75,6 @@ function cancelSlot(slotId) {
 
     request.onreadystatechange = function() {
         if(request.readyState == 4 && request.status == 200) {
-            console.log('success');
             getSlots();
         }
     }
