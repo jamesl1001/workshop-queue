951d577ac7438fd8175caa7672d0bf263acbb3ef
jamesl1001 <jamesl1001@msn.com>
2014-04-29 18:28:18 +0100

    Order by oldest slots first

--- php/Workshop.php
+++ php/Workshop.php
@@ -14,7 +14,7 @@ class Workshop {
     static function getWorkshopSlots($id) {
         require('db.php');
 
-        $sth = $dbh->query("SELECT slotId, name, kentId, location FROM slots WHERE workshopId='$id' ORDER BY slotId DESC");
+        $sth = $dbh->query("SELECT slotId, name, kentId, location FROM slots WHERE workshopId='$id'");
         $sth->setFetchMode(PDO::FETCH_OBJ);
         $result = $sth->fetchAll();
 
