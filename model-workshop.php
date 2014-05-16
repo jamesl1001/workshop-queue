ff52667714f9ec77044471bdb48b89e171a85421
jamesl1001 <jamesl1001@msn.com>
2014-05-16 22:29:46 +0100

    Only call getSlots() and generateRequestAssistanceMap() if  exists

--- model-workshop.php
+++ model-workshop.php
@@ -5,11 +5,11 @@ $workshopId = $url[1];
 
 require_once('php/Workshop.php');
 $workshop = Workshop::getData($workshopId);
-$slots    = Workshop::getSlots($workshopId);
-$map      = Workshop::generateRequestAssistanceMap($workshop->room);
 
 if($workshop) {
-    $file = 'workshop';
+    $file  = 'workshop';
+    $slots = Workshop::getSlots($workshopId);
+    $map   = Workshop::generateRequestAssistanceMap($workshop->room);
 } else {
     $file = '404';
 }
