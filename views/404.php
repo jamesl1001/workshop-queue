4483dca8f165aab7578cc7a50299a6ed913e7621
jamesl1001 <jamesl1001@msn.com>
2014-05-16 23:10:41 +0100

    Full-width sections on 404 page

--- views/404.php
+++ views/404.php
@@ -1,11 +1,15 @@
-<header class="centre">
-    <img src="/img/404.png" alt="404" id="header-img"/>
-    <div id="header-info">
-        <h1 id="header-title">404</h1>
-        <p>Page not found.</p>
+<header>
+    <div class="centre">
+        <img src="/img/404.png" alt="404" id="header-img"/>
+        <div id="header-info">
+            <h1 id="header-title">404</h1>
+            <p>Page not found.</p>
+        </div>
     </div>
 </header>
 
-<div id="main" class="centre">
-    <p>Sorry. The page you are looking for has not been found. Try checking the URL for errors or <a href="/">return to the homepage.</a></p>
+<div id="main">
+    <div class="centre">
+        <p>Sorry. The page you are looking for has not been found. Try checking the URL for errors or <a href="/">return to the homepage.</a></p>
+    </div>
 </div>
