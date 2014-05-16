467ac7aa5ab902a6d55eb31c7b9d98a387747fbe
jamesl1001 <jamesl1001@msn.com>
2014-05-16 23:06:29 +0100

    #header-title margin and font-size styles

--- front-view.php
+++ front-view.php
@@ -3,7 +3,7 @@
 <head>
     <?php require_once('includes/head.php'); ?>
 </head>
-<body>
+<body class="<?= ($isWorkshop) ? 'workshop' : ''; ?>">
     <?php require_once("views/$file.php"); ?>
     <?php require_once('includes/footer.php'); ?>
     <?php require_once('includes/scripts.php'); ?>
