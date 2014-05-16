3adf1a495f27fdd3cb7a77fcbcb5e23fdd3755eb
jamesl1001 <jamesl1001@msn.com>
2014-05-16 17:35:56 +0100

    Check whether mySlotId isset

--- php/getSlots.php
+++ php/getSlots.php
@@ -19,7 +19,7 @@ foreach($slots as $slot) {
     echo '<div class="user-slot" data-slotid="' . $slot->slotId . '" data-seat="' . $slot->seat . '">
               <span class="user-order">' . $i . '</span>
               <span class="user-info"><span class="user-name">' . $slot->name . '</span> <span class="user-id">' . $slot->kentId . '</span> <span class="user-seat">' . $slot->seat . '</span></span>';
-    if($slot->slotId == $_SESSION['mySlotId'] || (isset($_SESSION['admin']) && $_SESSION['admin'])) {
+    if((isset($_SESSION['mySlotId']) && $slot->slotId == $_SESSION['mySlotId']) || (isset($_SESSION['admin']) && $_SESSION['admin'])) {
         echo '<span class="user-cancel"><i class="icon-cancel"></i></span>';
     }
     echo '</div>';
