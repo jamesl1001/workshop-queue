4d1d5488af0f9ec5394bb264001e281be52d6109
jamesl1001 <jamesl1001@msn.com>
2014-09-18 19:22:31 +0100

    Use foreach key rather than i

--- php/getSlots.php
+++ php/getSlots.php
@@ -16,16 +16,12 @@ if($slots) {
     echo '<h2>This is the queue. ' . $slots[0]->name . ' is up next.</h2>';
 }
 
-$i = 1;
-
-foreach($slots as $slot) {
+foreach($slots as $key => $slot) {
     echo '<div class="user-slot" data-slotid="' . $slot->slotId . '" data-seat="' . $slot->seat . '">
-              <span class="user-order">' . $i . '</span>
+              <span class="user-order">' . ($key + 1) . '</span>
               <span class="user-info"><span class="user-name">' . $slot->name . '</span> <span class="user-seat">' . $slot->seat . '</span></span>';
     if((isset($_SESSION['mySlotId']) && $slot->slotId == $_SESSION['mySlotId']) || (isset($_SESSION['admin']) && $_SESSION['admin'])) {
         echo '<span class="user-cancel"><i class="icon-cancel"></i></span>';
     }
     echo '</div>';
-
-    $i++;
 }
