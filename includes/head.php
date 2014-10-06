9902597fcf86948dc50366f9e0622872bfdbeb8b
jamesl1001 <jamesl1001@msn.com>
2014-10-06 21:54:09 +0100

    Use explicit font-weight values for better cross-browser consistency

--- includes/head.php
+++ includes/head.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 <title>Workshop Queue</title>
 <link rel="stylesheet" type="text/css" href="/css/styles.css"/>
-<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans">
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600">
 <link rel="shortcut icon" href="/img/favicon.ico"/>
 <link rel="apple-touch-icon-precomposed" href="/img/touchicon-57.png">
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/touchicon-72.png">
