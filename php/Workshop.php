81a2e2a918a94ab3c4bf8f59bcead36d8692763b
James Alexander Lee <jamesl1001@msn.com>
2014-10-06 16:06:50 +0100

    Only display active slots

--- php/Workshop.php
+++ php/Workshop.php
@@ -14,7 +14,7 @@ class Workshop {
     static function getSlots($id) {
         require('db.php');
 
-        $sth = $dbh->query("SELECT slotId, name, seat FROM slots WHERE workshopId='$id'");
+        $sth = $dbh->query("SELECT slotId, name, seat FROM slots WHERE workshopId='$id' AND active=1");
         $sth->setFetchMode(PDO::FETCH_OBJ);
         $result = $sth->fetchAll();
 
