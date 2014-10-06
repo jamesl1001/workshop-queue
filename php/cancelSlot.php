3dfb18b7628f3a761dc822723623e54b3d4568ca
James Alexander Lee <jamesl1001@msn.com>
2014-10-06 15:46:58 +0100

    Store timestamp when assistance is cancelled

--- php/cancelSlot.php
+++ php/cancelSlot.php
@@ -4,6 +4,8 @@ require('db.php');
 
 $slotId = $_POST['slotId'];
 
-$sth = $dbh->prepare("DELETE FROM slots WHERE slotId=:slotId");
-$sth->bindParam(':slotId', $slotId);
-$result = $sth->execute();
+$cancelled = date("Y-m-d H:i:s");
+
+$sth = $dbh->prepare("UPDATE slots SET active=0, cancelled=:cancelled WHERE slotId=$slotId");
+$sth->bindParam(':cancelled', $cancelled);
+$sth->execute();
