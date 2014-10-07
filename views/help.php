1d64a9cb33d8e6bc0786c9e96620295aa88447eb
James Alexander Lee <jamesl1001@msn.com>
2014-10-07 17:51:00 +0100

    Add the Help page

---
+++ views/help.php
@@ -0,0 +1,55 @@
+<header>
+    <div class="centre">
+        <img src="/img/help.png" alt="Question mark" id="header-img"/>
+        <div id="header-info">
+            <h1 id="header-title">Help</h1>
+            <p>How to request assistance.</p>
+        </div>
+        <a href="javascript:history.back()" id="exit-btn" title="Back"><i class="icon-exit"></i></a>
+    </div>
+</header>
+
+<div id="main">
+    <div class="centre">
+        <ol>
+            <li>
+                <p>Find which workshop you are in on the <a href="/">home page</a>.</p>
+                <p>Information such as the course code and title, lecturer, workshop date and room are provided to help you find the workshop you are attending.</p>
+                <div class="help-screenshot">
+                    <img src="/img/admin.png" alt="Home page"/>
+                </div>
+            </li>
+            <li>
+                <p>Locate the 'Request assistance' form. On desktop, this will be in the right-hand column. On mobile or tablet, it may be hidden until you click the '+ Request assistance' button at the bottom of the page.</p>
+                <div class="help-screenshot">
+                    <img src="/img/help-request-assistance.jpg" alt="Request assistance"/>
+                </div>
+            </li>
+            <li>
+                <p>Enter your name.</p>
+                <div class="help-screenshot">
+                    <img src="/img/help-name.jpg" alt="Name"/>
+                </div>
+            </li>
+            <li>
+                <p>Select your seat / computer name. This will be shown on the front of the PC case.</p>
+                <div class="help-screenshot">
+                    <img src="/img/help-pc-name.jpg" alt="Computer name shown on the front of the PC case"/>
+                </div>
+            </li>
+            <li>
+                <p>Click 'Request'. You have now been added to the end of the queue.</p>
+                <p>The queue will update automatically so you don't need to worry about refreshing the page.</p>
+                <div class="help-screenshot">
+                    <img src="/img/help-request.jpg" alt="Request"/>
+                </div>
+            </li>
+            <li>
+                <p>If your assistance is no longer required, you can cancel your request by clicking the × button.</p>
+                <div class="help-screenshot">
+                    <img src="/img/help-cancel-request.jpg" alt="Cancel request"/>
+                </div>
+            </li>
+        </ol>
+    </div>
+</div>
