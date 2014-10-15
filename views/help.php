d9b4e158db19c8a13738a8f587101e087784b055
James Alexander Lee <jamesl1001@msn.com>
2014-10-15 11:42:37 +0100

    Add the Help images

--- views/help.php
+++ views/help.php
@@ -16,19 +16,19 @@
                 <p>Find which workshop you are in on the <a href="/">home page</a>.</p>
                 <p>Information such as the course code and title, lecturer, workshop date and room are provided to help you find the workshop you are attending.</p>
                 <div class="help-screenshot">
-                    <img src="/img/admin.png" alt="Home page"/>
+                    <img src="/img/help-workshops.png" alt="Workshops on the home page"/>
                 </div>
             </li>
             <li>
                 <p>Locate the 'Request assistance' form. On desktop, this will be in the right-hand column. On mobile or tablet, it may be hidden until you click the '+ Request assistance' button at the bottom of the page.</p>
                 <div class="help-screenshot">
-                    <img src="/img/help-request-assistance.jpg" alt="Request assistance"/>
+                    <img src="/img/help-request-assistance.png" alt="Request assistance"/>
                 </div>
             </li>
             <li>
                 <p>Enter your name.</p>
                 <div class="help-screenshot">
-                    <img src="/img/help-name.jpg" alt="Name"/>
+                    <img src="/img/help-name.png" alt="Name"/>
                 </div>
             </li>
             <li>
@@ -36,18 +36,21 @@
                 <div class="help-screenshot">
                     <img src="/img/help-pc-name.jpg" alt="Computer name shown on the front of the PC case"/>
                 </div>
+                <div class="help-screenshot">
+                    <img src="/img/help-select-seat.png" alt="Select seat"/>
+                </div>
             </li>
             <li>
                 <p>Click 'Request'. You have now been added to the end of the queue.</p>
                 <p>The queue will update automatically so you don't need to worry about refreshing the page.</p>
                 <div class="help-screenshot">
-                    <img src="/img/help-request.jpg" alt="Request"/>
+                    <img src="/img/help-slots.png" alt="Slots"/>
                 </div>
             </li>
             <li>
                 <p>If your assistance is no longer required, you can cancel your request by clicking the × button.</p>
                 <div class="help-screenshot">
-                    <img src="/img/help-cancel-request.jpg" alt="Cancel request"/>
+                    <img src="/img/help-cancel-request.png" alt="Cancel request"/>
                 </div>
             </li>
         </ol>
